97f6e95241bedfe08cac0bd854f8bb01141d151a
Mohammad ali Sharifi <31005113+mash021@users.noreply.github.com>
2022-12-10 15:57:52 +0330

    Update FileNameChange.py

--- FileNameChange.py
+++ FileNameChange.py
@@ -1,7 +1,14 @@
 import os
+import tkinter as tk
+from tkinter import filedialog
+import time
 
 
-
-directory = '<your path>'
-
-[os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('_', ' ').lower()) for f in os.listdir(directory)]
+root = tk.Tk()
+root.withdraw()
+# file_path = filedialog.askopenfilename()
+directory = filedialog.askdirectory()
+[os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('_', ' ').lower()) for f in
+ os.listdir(directory)]
+time.sleep(5)
+print("Done!")
