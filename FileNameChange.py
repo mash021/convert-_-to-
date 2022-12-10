e3bca102383503c3b5e0d91fe41f64feea2ae3e6
Mohammad ali Sharifi <31005113+mash021@users.noreply.github.com>
2022-12-10 15:56:59 +0330

    Update FileNameChange.py

--- FileNameChange.py
+++ FileNameChange.py
@@ -1,5 +1,7 @@
 import os
 
-directory = 'icon'
+
+
+directory = '<your path>'
 
 [os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('_', ' ').lower()) for f in os.listdir(directory)]
